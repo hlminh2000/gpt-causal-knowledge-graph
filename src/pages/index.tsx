006fcf9acf60c6e7ad8021475394ef1f4e73135a
Minh Ha <hlminh2000@gmail.com>
2023-04-21 16:41:32 +0000

    initial UI implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,37 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Textarea from '@mui/joy/Textarea';
+import { Button, Container, Grid, Input } from '@mui/joy';
+import { useState } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+type KnowledgeGraph = {
+  nodes: {
+    id: string,
+    text: string,
+    embeddings: any,
+  }[],
+  edges: {
+    cause: KnowledgeGraph["nodes"][0]["id"]
+    effect: KnowledgeGraph["nodes"][0]["id"]
+  }[]
+}
 
 export default function Home() {
+
+  const [storyText, setStoryText] = useState<string>()
+  const [openAiApiKey, setOpenAiApiKey] = useState<string>()
+  const [knowledgeGraph, setKnowledgeGraph] = useState<KnowledgeGraph>({
+    nodes: [],
+    edges: []
+  })
+
+  const onSubmit = () => {
+    fetch("/api/submit", {
+      method: "POST",
+      body: JSON.stringify({ storyText })
+    })
+  }
+
   return (
     <>
       <Head>
@@ -15,99 +41,30 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
+        <Container className={styles.center}>
+          <Grid container xs={12} spacing={2}>
+            <Grid xs={12}>
+              <Input
+                placeholder='OpenAI API Key' 
+                type="password" 
+                value={openAiApiKey} 
+                onChange={e => setOpenAiApiKey(e.target.value)} 
               />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+            </Grid>
+            <Grid xs={12}>
+              <Textarea 
+                placeholder="Type a story..."
+                value={storyText}
+                onChange={e => setStoryText(e.target.value)}
+                size='lg'
+                sx={{width: "100%"}}
+              />
+            </Grid>
+            <Grid>
+              <Button onClick={onSubmit} disabled={!storyText || !openAiApiKey}> Create </Button>
+            </Grid>
+          </Grid>
+        </Container>
       </main>
     </>
   )
